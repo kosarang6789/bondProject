baf328a05c96d2d70433b9003d80d5ab2742d58b
cymakp <chimakpi0804@gmail.com>
2022-11-28 00:31:39 +0900

    1127 신고 기능 만들기(진행중)

---
+++ Bond/src/main/java/kh/semi/project/admin/model/vo/Report.java
@@ -0,0 +1,14 @@
+package kh.semi.project.admin.model.vo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Setter
+@Getter
+public class Report {
+	
+	private int reasonCode;
+	private String reasonDetails;
+}
