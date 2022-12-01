b178ada64230124c9d6d61d7fedfb8bac0281f2e
Seo-de <gkw1590@gmail.com>
2022-12-01 09:59:22 +0900

    20221201-본드 멤버 매퍼 수정

---
+++ Bond/src/main/java/kh/semi/project/bond/model/vo/GroupMemberList.java
@@ -0,0 +1,20 @@
+package kh.semi.project.bond.model.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class GroupMemberList {
+	
+	private int joinNo;
+	private int groupNo;
+	private int memberNo;
+	private String leaderYN;
+	private String joinDate;
+	private String joinStatus;
+	private String leaveDate;
+
+}
