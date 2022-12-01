8d411b2bb26e141c21903c973eb5882a5b84e328
cy-shin <chimakpi0804@gmail.com>
2022-12-01 12:48:41 +0900

    1201 일정만들기

--- Bond/src/main/webapp/WEB-INF/views/plan/plan.jsp
+++ Bond/src/main/webapp/WEB-INF/views/plan/plan.jsp
@@ -9,9 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>달력 연습</title>
 
-    <!-- fullcalendar cdn -->
-    <script src="https://cdn.jsdelivr.net/combine/npm/fullcalendar@5.11.3/main.min.js,npm/fullcalendar@5.11.3,npm/fullcalendar@5.11.3/locales-all.min.js,npm/fullcalendar@5.11.3/locales-all.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/combine/npm/fullcalendar@5.11.3/main.min.css,npm/fullcalendar@5.11.3/main.min.css">
+    
     
     <!-- css -->
     <link rel="stylesheet" href="/resources/css/plan/plan-style.css">
@@ -36,16 +34,17 @@
           <span id="closeModalButton">&times;</span>
         </div> <!-- end window-header -->
 
-        <div class="window-body">
+        <div id="windowBody">
 
           <div id="titleBox">
-            제목 : <input type="text" id="inputTitle">
+            <%-- 제목 : 
+            <input type="text" id="inputTitle"> --%>
           </div> <!-- end titleBox -->
 
           <div id="colorBox">
-            색깔
+            <%-- 색깔
             <label>
-              <input type="radio" name="planColor" value="rgb(255,60,45)">
+              <input type="radio" name="planColor" value="rgb(255,60,45)" checked>
               <div class="palette" style="background-color:rgb(255,60,45)"></div>
             </label>
 
@@ -57,21 +56,20 @@
             <label>
               <input type="radio" name="planColor" value="rgb(55,136,216)">
               <div class="palette" style="background-color:rgb(55,136,216)"></div>
-            </label>
-
+            </label> --%>
           </div>
   
           <div id="dateBox">
-            <div class="dateBoxRow">
-              시작 날짜 <input type="date" id="inputStartDate"> <input type="time" id="inputStartTime">
+            <div class="dateBoxRow" id="startBox">
+              <%-- 시작 날짜 <input type="date" id="inputStartDate"> <input type="time" id="inputStartTime"> --%>
             </div>
-            <div class="dateBoxRow">
-              종료 날짜 <input type="date" id="inputEndDate">   <input type="time" id="inputEndTime">
+            <div class="dateBoxRow" id="endBox">
+              <%-- 종료 날짜 <input type="date" id="inputEndDate">   <input type="time" id="inputEndTime"> --%>
             </div>
           </div> <!-- end dateBox -->
   
           <div id="contentBox">
-            내용 : <input type="text" id="inputContent">
+            <%-- 내용 : <input type="text" id="inputContent"> --%>
           </div> <!-- end commentBox -->
   
           <div id="buttonBox">
@@ -85,10 +83,19 @@
 
     </div> <!-- end modal -->
 
+
+
+    <%-- CDN --%>
+
+    <!-- fullcalendar CDN 방식으로 추가 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/combine/npm/fullcalendar@5.11.3/main.min.css,npm/fullcalendar@5.11.3/main.min.css">
+    <script src="https://cdn.jsdelivr.net/combine/npm/fullcalendar@5.11.3/main.min.js,npm/fullcalendar@5.11.3,npm/fullcalendar@5.11.3/locales-all.min.js,npm/fullcalendar@5.11.3/locales-all.min.js"></script>
+
     <%-- jQuery CDN 방식으로 추가 --%>
     <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 
-    <!-- script -->
+    <!-- JS script -->
     <script src="/resources/js/plan/plan.js"></script>
+
   </body>
 </html>
--- Bond/src/main/webapp/resources/js/plan/plan.js
+++ Bond/src/main/webapp/resources/js/plan/plan.js
@@ -1,108 +1,234 @@
-document.addEventListener('DOMContentLoaded', function() {
+// fullcalendar 불러오기
+function loadCalendar(){ 
+    // document.addEventListener('DOMContentLoaded', () => {
 
-    (() => {
-        ajax()
-    })()
+        // fullcalendar 관련 js
+        var calendarEl = document.getElementById('calendar');
+
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            locale: 'ko',
+            initialView: 'dayGridMonth',
+
+            headerToolbar : {
+                left: 'title',
+                center: 'prev next today',
+                right: 'addEventButton'
+            },
+            customButtons : {
+                addEventButton : {
+                text: '일정 추가하기',
+                click: function() {
+
+                    // 열려라 모달창
+                    const modal = document.getElementById("modal");
+                    modal.style.display="block";
+
+                    makeWindowBody();
+                }
+                }
+            },
+            
+        });
+
+        // 달력에 정보를 뿌리는 함수 getAllPlan
+        function getAllPlan(){
+            $.ajax({
+                url : "/plan/list",
+                data : {"groupNo": 1},
+                type : "POST",
+                dataType : "JSON",
+
+                success : (calenderList) => {
+                for(let item of calenderList) {
+                    console.log(item);
+                    calendar.addEvent({
+                        title: item.planTitle,
+                        start: item.planStart,
+                        end : item.planEnd,
+                        allDay: true,
+                        backgroundColor: item.planColor
+                    });
+                }
+                }
+            })
+        }
+
+        // 처음 이동 시 달력 정보를 전부 가져옴
+        (() => {
+            getAllPlan()
+        })()
+
+    // 캘린더 렌더링
+    calendar.render();
+
+    // }); // end full calander
+}
+
+(() => {
+    loadCalendar();
+})()
+
+
+// 닫혀라 모달창 함수
+const closeModalButton = document.getElementById("closeModalButton");
+
+closeModalButton.addEventListener("click", () => {
+    modal.style.display="none";
+    clearWindowBody();
+})
+
+// 모달창에서 확인 버튼을 누르면 일정을 업데이트함
+const confirmButton = document.getElementById("confirmButton");
+
+confirmButton.addEventListener("click", () => {
+
+    const inputTitle = document.getElementById("inputTitle").value;
+    const inputContent = document.getElementById("inputContent").value;
+    const inputStartDate = document.getElementById("inputStartDate").value;
+    const inputStartTime = document.getElementById("inputStartTime").value;
+    const inputEndDate = document.getElementById("inputEndDate").value;
+    const inputEndTime = document.getElementById("inputEndTime").value;
+    const inputColor = document.querySelector("input[name='planColor']:checked").value;
+
+    // 비동기로 일정 업데이트(ajax)
+    $.ajax({
+        url : "/plan/insert",
+        data : {
+                "inputTitle" : inputTitle,
+                "inputContent" : inputContent,
+                "inputStartDate" : inputStartDate,
+                "inputStartTime" : inputStartTime,
+                "inputEndDate" : inputEndDate,
+                "inputEndTime" : inputEndTime,
+                "inputColor" : inputColor
+                },
+        type : "POST",
+        dataType : "JSON",
+
+        success : (message) => {
+            loadCalendar();
+
+            // 성공 시 화면에 정보를 다시 뿌림
+            alert(message);
+        },
+        error : (message) => {
+            alert(message);
+        }
+    })
+
+    // 결과와 상관 없이 모달창을 닫고 초기화함
+    modal.style.display="none";
+    clearWindowBody();
+})
+
+
+
+
+// windowBody 화면 만들기 함수
+function makeWindowBody(){
+
+    // 1. titleBox
+    const titleBox = document.getElementById("titleBox");
+    const inputTitle = document.createElement("input");
+    inputTitle.id = 'inputTitle';
+
+    const titleBoxSpan = document.createElement("span");
+    titleBoxSpan.innerText = '제목';
+
+    titleBox.append(titleBoxSpan, inputTitle);
+
+    // 2. colorBox
+    const colorBox = document.getElementById("colorBox");
+
+    const colorBoxSpan = document.createElement("span");
+    colorBoxSpan.innerText = '색깔';
+
+    colorBox.append(colorBoxSpan);
     
-    function ajax(){
-        $.ajax({
-            url : "/plan/list",
-            data : {"groupNo": 1},
-            type : "POST",
-            dataType : "JSON",
-    
-            success : (calenderList) => {
-            for(let item of calenderList) {
-                console.log(calenderList);
-                calendar.addEvent({
-                    title: item.planTitle,
-                    start: item.planStart,
-                    end : item.planEnd,
-                    allDay: true,
-                    backgroundColor: item.planColor
-                });
-            }
-            }
-        })
+    const paletteArr = new Array(); // 색깔 배열
+    paletteArr.push("rgb(255,60,45)", "rgb(240,200,55)", "rgb(55,136,216)"); // 색깔을 추가하세요
+
+    for(let i=0; i<paletteArr.length; i++) {
+        const label = document.createElement("label");
+        const radio = document.createElement("input");
+        radio.setAttribute("type", "radio");
+        radio.setAttribute("name", "planColor");
+        radio.setAttribute("value", paletteArr[i]);
+
+        if(i==0) {
+            radio.checked
+        }
+
+        const palette = document.createElement("div");
+        palette.classList.add("palette");
+        palette.setAttribute("style", "background-color:"+ paletteArr[i]);
+
+        label.append(radio, palette);
+        colorBox.append(label);
     }
 
-// fullcalendar 관련
-var calendarEl = document.getElementById('calendar');
+    // 3. startBox
+    const startBox = document.getElementById("startBox");
 
-var calendar = new FullCalendar.Calendar(calendarEl, {
-    locale: 'ko',
-    initialView: 'dayGridMonth',
+    const inputStartDate = document.createElement("input");
+    inputStartDate.setAttribute("type", "date");
+    inputStartDate.id = "inputStartDate";
 
-    headerToolbar : {
-        left: 'title',
-        center: 'prev next today',
-        right: 'addEventButton'
-    },
-    customButtons : {
-        addEventButton : {
-        text: '일정 추가하기',
-        click: function() {
-            // 모달 열기 이벤트
-            const modal = document.getElementById("modal");
-            modal.style.display="block";
+    const inputStartTime = document.createElement("input");
+    inputStartTime.setAttribute("type", "time");
+    inputStartTime.id = "inputStartTime";
 
-            // 모달 닫기 이벤트
-            const closeModalButton = document.getElementById("closeModalButton");
+    const startBoxSpan = document.createElement("span");
+    startBoxSpan.innerText = '시작날짜';
 
-            closeModalButton.addEventListener("click", () => {
-                modal.style.display="none"
-            })
+    startBox.append(startBoxSpan, inputStartDate, inputStartTime);
 
-            // 확인 버튼 클릭 시 이벤트
-            const confirmButton = document.getElementById("confirmButton");
+    // 4. endBox
+    const endBox = document.getElementById("endBox");
 
-            confirmButton.addEventListener("click", () => {
-                const inputTitle = document.getElementById("inputTitle").value; /* 제목 */
-                const inputContent = document.getElementById("inputContent").value; /* 내용 */
-                const inputStartDate = document.getElementById("inputStartDate").value; /* 시작 날짜 */
-                const inputStartTime = document.getElementById("inputStartTime").value; /* 시작 시간 */
-                const inputEndDate = document.getElementById("inputEndDate").value; /* 종료 날짜 */
-                const inputEndTime = document.getElementById("inputEndTime").value; /* 종료 시간 */
-                const inputColor = document.querySelector("input[name='planColor']:checked").value; /* 종료 시간 */
+    const inputEndDate = document.createElement("input");
+    inputEndDate.setAttribute("type", "date");
+    inputEndDate.id = "inputEndDate";
 
-                $.ajax({
-                    url : "/plan/insert",
-                    data : {
-                            "inputTitle" : inputTitle,
-                            "inputContent" : inputContent,
-                            "inputStartDate" : inputStartDate,
-                            "inputStartTime" : inputStartTime,
-                            "inputEndDate" : inputEndDate,
-                            "inputEndTime" : inputEndTime,
-                            "inputColor" : inputColor
-                            },
-                    type : "POST",
-                    dataType : "JSON",
+    const inputEndTime = document.createElement("input");
+    inputEndTime.setAttribute("type", "time");
+    inputEndTime.id = "inputEndTime";
 
-                    success : (message) => {
-                        alert(message)
-                    },
-                    error : () => {
-                        alert("실패")
-                    }
-                })
+    const endBoxSpan = document.createElement("span");
+    endBoxSpan.innerText = '종료날짜';
 
-                // // 날짜 형식을 fullcalander에 맞게 변환
-                // var startDate = new Date(inputStartDate + 'T' + inputStartTime); // will be in local time
-                // var endDate = new Date(inputEndDate + 'T' + inputEndTime); // will be in local time
-                
-                modal.style.display="none";
-                
-                // 화면에 정보 다시 뿌리기
-                ajax();
+    endBox.append(endBoxSpan, inputEndDate, inputEndTime);
 
-            })
+    // 5. contentBox
+    const contentBox = document.getElementById("contentBox");
 
-        }
-        }
-    },
-    
-});
+    const inputContent = document.createElement("input");
+    inputContent.id = "inputContent";
+
+    const contentBoxSpan = document.createElement("span");
+    contentBoxSpan.innerText = '내용';
+
+    contentBox.append(contentBoxSpan, inputContent);
+
+}
+
+// windowBody 비우기 함수
+function clearWindowBody(){
+    const titleBox = document.getElementById("titleBox");
+    const colorBox = document.getElementById("colorBox");
+    const startBox = document.getElementById("startBox");
+    const endBox = document.getElementById("endBox");
+    const contentBox = document.getElementById("contentBox");
+
+    titleBox.innerHTML = "";
+    colorBox.innerHTML = "";
+    startBox.innerHTML = "";
+    endBox.innerHTML = "";
+    contentBox.innerHTML = "";
+}
+
+
+
 
 // 캘린더 예시
 // calendar.addEvent({
@@ -112,11 +238,3 @@ var calendar = new FullCalendar.Calendar(calendarEl, {
 //     allDay: true,
 //     backgroundColor: 'red'
 // });
-
-// 달력에 정보 뿌리기...
-
-
-
-calendar.render();
-
-});
