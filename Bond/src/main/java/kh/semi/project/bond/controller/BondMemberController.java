e1d58e936a7c1ee347782e9a9bcce4f9ae2b055e
Seo-de <110797113+Seo-de@users.noreply.github.com>
2022-12-01 23:45:41 +0900

    20221201-멤버 그룹 리스트 불러오기
    리스트 불러오기가 안되네....

--- Bond/src/main/java/kh/semi/project/bond/controller/BondMemberController.java
+++ Bond/src/main/java/kh/semi/project/bond/controller/BondMemberController.java
@@ -43,7 +43,7 @@ public class BondMemberController {
 		
 		Map<String, Object> memMap = serivce.selectMemberList(groupNo);
 		
-		model.addAttribute(memMap);
+		model.addAttribute("memMap", memMap);
 
 		
 		return "bond/bondMemberList";
