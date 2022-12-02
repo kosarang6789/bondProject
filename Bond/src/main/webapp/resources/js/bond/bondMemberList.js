a88b2049a50ae0e3fe40cb733f7b1a6de9fabd3e
Seo-de <gkw1590@gmail.com>
2022-12-02 17:48:11 +0900

    20221202-본드 멤버 리스트
    무한스크롤 작성 중

--- Bond/src/main/webapp/resources/js/bond/bondMemberList.js
+++ Bond/src/main/webapp/resources/js/bond/bondMemberList.js
@@ -21,31 +21,51 @@ window.onclick=function(e){
 
 // 무한스크롤
 
-// const option = {
-//     root: null,
-//     rootMargind: "0px 0px 0px 0px",
-//     thredhold: 1
-// }
+const option = {
+    root: null,
+    rootMargind: "0px 0px 0px 0px",
+    thredhold: 1
+}
 
-// const observer = new IntersectionObserver(selectBoardScroll, option);
-// const target = document.getElementById("pageTarget");
-// observer.observe(target);
+const observer = new IntersectionObserver(selectBoardScroll, option);
+const target = document.getElementById("pageTarget");
+observer.observe(target);
 
-// let flag = true;
-// function selectBoardScroll (){
-//     if(cp==0){ cp++; }
-//     else{
-//         if(flag){
-//             $.ajax({
-//                 url : "/bondMemberList",
-//                 data : {"cp":++cp},
-//                 dataType : "JSON",
-//                 success : function(map){
-//                     if(map != null){
-//                         console.log(map.mem);
-//                     }
-//                 }
-//             })
-//         }
-//     }
-// } 
+let flag = true;
+function selectBoardScroll (){
+    if(cp==0){ cp++; }
+    else{
+        if(flag){
+            $.ajax({
+                url : "/bondMemberList",
+                data : {"cp":++cp},
+                dataType : "JSON",
+                success : function(memMap){
+                    if(memMap != null){
+                        console.log(memMap);
+                        
+                            const memberAll = document.querySelector("memberAll");
+
+                        for(let mem of memMap.memList){
+                            const memberList = document.createElement("div");
+                            memberList.classList.add(".member-image");
+
+                            const memberImage = document.createElement("div");
+                            memberImage.classList.add(".member-image");
+
+                            const memberName = document.createElement("div");
+                            memberName.classList.add(".member-name");
+
+                            const memberReport = document.createElement("div");
+                            memberReport.classList.add("member-report");
+                        }
+                        
+                    }
+                },
+                error : ()=>{
+                    alert("ajax 통신 오류");
+                }
+            })
+        }
+    }
+} 
