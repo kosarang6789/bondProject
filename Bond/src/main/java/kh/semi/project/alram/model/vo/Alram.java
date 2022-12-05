4544bbbcd18500d23bf3176e05bfbf6aa43903c3
Seo-de <gkw1590@gmail.com>
2022-12-05 15:14:51 +0900

    20221205-멤버 프로필 css 수정

--- Bond/src/main/java/kh/semi/project/alram/model/vo/Alram.java
+++ Bond/src/main/java/kh/semi/project/alram/model/vo/Alram.java
@@ -1,5 +1,19 @@
 package kh.semi.project.alram.model.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Alram {
+	
+	private int groupNo; // 그룹 번호
+	private int myNo; // 알림 받을 로그인 멤버 번호
+	private int memberNo; // 알림 보낸 사람 번호
+	
+	private int postNo; // 좋아요 누른 게시글 번호
+	private int commentNo; // 댓글 단
 
 }
