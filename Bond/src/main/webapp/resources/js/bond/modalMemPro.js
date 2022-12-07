c804ce73f22e3de4e731d0894a874b72c864265f
Seo-de <110797113+Seo-de@users.noreply.github.com>
2022-12-08 01:56:37 +0900

    20221208-필요없는 주석 지움

--- Bond/src/main/webapp/resources/js/bond/modalMemPro.js
+++ Bond/src/main/webapp/resources/js/bond/modalMemPro.js
@@ -1,7 +1,7 @@
 // ---------회원 정보 모달 창------------
 
 // 밴드 게시글의 프로필 이미지 모달
-// if(document.querySelector(".uprofile") != null){
+if(document.querySelector(".uprofile") != null){
     let images = document.getElementsByClassName("uprofile");
 
     // 멤버 이미지 불러와 for문 안에 넣어 id값 얻고 모달창 띄움
@@ -13,26 +13,10 @@
             
         });
     };
-// }
-
-// // 밴드 게시글 상세조회 프로필 이미지 모달
-// if(document.querySelector(".postMember-img") != null){
-//     let images = document.getElementsByClassName("uprofile");
-
-//     // 멤버 이미지 불러와 for문 안에 넣어 id값 얻고 모달창 띄움
-//     for(let m of images){
-//         m.addEventListener("click", ()=>{
-//             modalMemPro.classList.add("show");
-//             modalMemPro.classList.remove("hidden");
-//             selectMemPro(m.getAttribute("id"));
-            
-//         });
-//     };
-// }
+}
 
 const modalMemPro = document.querySelector(".modalMemPro");
 const modalx = document.querySelector(".modalx");
-
 
 
 modalx.addEventListener("click",()=>{
